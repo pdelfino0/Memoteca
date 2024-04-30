85d0da350dcdebc78d4fa748071a32ef28d7f117
Pedro <pedrofernandesdelfino@gmail.com>
2024-04-30 18:07:39 -0300

    Event Binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Component} from '@angular/core';
+import {RouterOutlet} from '@angular/router';
+import {HeaderComponent} from "./components/header/header.component";
+import {FooterComponent} from "./components/footer/footer.component";
+import {CriarPensamentoComponent} from "./components/criar-pensamento/criar-pensamento.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent, CriarPensamentoComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
