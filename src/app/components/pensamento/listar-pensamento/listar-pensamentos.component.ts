7b15f9853b2430afd0a11e23bcd90db573b507c3
Pedro <pedrofernandesdelfino@gmail.com>
2024-05-11 15:11:04 -0300

    ngFor and @Input on component son to render data from component father

--- src/app/components/pensamento/listar-pensamento/listar-pensamentos.component.ts
+++ src/app/components/pensamento/listar-pensamento/listar-pensamentos.component.ts
@@ -1,15 +1,32 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 import {RouterLink} from "@angular/router";
+import {PensamentoComponent} from "../pensamento/pensamento.component";
+import {NgForOf} from "@angular/common";
 
 @Component({
   selector: 'app-listar-pensamento',
   standalone: true,
   imports: [
-    RouterLink
+    RouterLink,
+    PensamentoComponent,
+    NgForOf
   ],
   templateUrl: './listar-pensamentos.component.html',
   styleUrl: './listar-pensamentos.component.css'
 })
 export class ListarPensamentosComponent {
+
+  listaPensamentos = [
+    {
+      conteudo: 'Passo propriedades para o componente filho',
+      autoria: 'Componente pai',
+      modelo: 'modelo3'
+    },
+    {
+      conteudo: 'Minha propriedade é decorada com @Input()',
+      autoria: 'Componente filho',
+      modelo: 'modelo2'
+    },
+  ]
 
 }
---
+++ src/app/components/pensamento/pensamento/pensamento.component.ts
@@ -0,0 +1,13 @@
+import {Component, Input} from '@angular/core';
+
+@Component({
+  selector: 'app-pensamento',
+  standalone: true,
+  imports: [],
+  templateUrl: './pensamento.component.html',
+  styleUrl: './pensamento.component.css'
+})
+export class PensamentoComponent {
+
+  @Input() pensamento = {}
+}
