11c711fa843e136891cd5c6b0dfeb5824bea17b9
Pedro <pedrofernandesdelfino@gmail.com>
2024-05-11 16:27:32 -0300

    ngClass using a .ts function to set the class based on logical decisions

--- src/app/components/pensamento/listar-pensamento/listar-pensamentos.component.ts
+++ src/app/components/pensamento/listar-pensamento/listar-pensamentos.component.ts
@@ -18,16 +18,22 @@ import {NgForOf, NgIf} from "@angular/common";
 export class ListarPensamentosComponent {
 
   listaPensamentos = [
-    // {
-    //   conteudo: 'Passo propriedades para o componente filho',
-    //   autoria: 'Componente pai',
-    //   modelo: 'modelo3'
-    // },
-    // {
-    //   conteudo: 'Minha propriedade é decorada com @Input()',
-    //   autoria: 'Componente filho',
-    //   modelo: 'modelo2'
-    // },
+    {
+      conteudo: 'Passo propriedades para o componente filho',
+      autoria: 'Componente pai',
+      modelo: 'modelo3'
+    },
+    {
+      conteudo: 'Minha propriedade é decorada com @Input()',
+      autoria: 'Componente filho',
+      modelo: 'modelo2'
+    },
+
+    {
+      conteudo: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus quis imperdiet eros. Suspendisse a nisl consectetur tellus semper efficitur. Donec tincidunt, nibh a placerat pharetra, orci justo molestie purus, ac porta augue velit quis lacus. Ut tristique lacus dolor, quis tristique dui finibus eget. Nulla scelerisque bibendum urna eu volutpat. Cras sed dignissim sem. Pellentesque eu dui pulvinar, varius arcu dictum, pharetra turpis. Sed tristique aliquam porta. Pellentesque ullamcorper nec mauris at varius. Sed sapien massa, egestas ornare congue sit amet, mollis eget mi. Donec gravida lacus metus, non vestibulum dui aliquam a. Ut lacus justo, ullamcorper nec sapien at, tempor ullamcorper sapien. ',
+      autoria: 'Componente filho',
+      modelo: ''
+    }
   ]
 
 }
--- src/app/components/pensamento/pensamento/pensamento.component.ts
+++ src/app/components/pensamento/pensamento/pensamento.component.ts
@@ -1,18 +1,25 @@
 import {Component, Input} from '@angular/core';
+import {NgClass} from "@angular/common";
 
 @Component({
   selector: 'app-pensamento',
   standalone: true,
-  imports: [],
+  imports: [
+    NgClass
+  ],
   templateUrl: './pensamento.component.html',
   styleUrl: './pensamento.component.css'
 })
 export class PensamentoComponent {
 
+  larguraPensamento(): string{
+    return this.pensamento.conteudo.length> 256 ? 'pensamento-g' : 'pensamento-p'
+  }
   @Input() pensamento = {
     conteudo: '',
     autoria: '',
     modelo: ''
   }
+
 
 }
